47649d4b006c01386bad5da857ba4bc046387fc4
Ryan DJ <ryan.djoenaidi@akqa.com>
2024-04-07 14:07:41 +1200

    Change livewire component to blade component

--- resources/views/components/links.blade.php
+++ resources/views/components/links.blade.php
@@ -9,7 +9,7 @@
                         : 'border-gray-100 dark:border-gray-700 text-gray-400 dark:text-gray-400';
             @endphp
 
-            <div wire:click="$parent.switchTab('{{ $tabs->id }}', '{{ $tabLink->id }}')" key="{{ $tabLink->id }}"
+            <div wire:click="switchTab('{{ $tabs->id }}', '{{ $tabLink->id }}')" key="{{ $tabLink->id }}"
                 class='px-6 py-4 cursor-pointer border-b-2 grow hover:text-gray-700 dark:hover:text-gray-100 {{ $linkClasses }}'>
                 {{ $tabLink->label }}
             </div>
